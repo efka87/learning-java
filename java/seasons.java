705a9a6716762071aa99d3d101dc4417e75c35f3
efka87 <naanuri@hotmail.com>
2024-04-04 15:57:51 +0300

    seasons test

---
+++ java/seasons.java
@@ -0,0 +1,19 @@
+public class seasons {
+    public static void main (String[] args) {
+      // winter, spring, summer, autumn
+      // warm jacket, t-shirt, swimming suite, rain coat
+        String season = "spring";
+  
+        if (season == "winter") {
+            System.out.println("Wear a warm jacket!");
+        } else if (season == "spring") {
+            System.out.println("Wear a T-shirt!");
+        } else if (season == "summer") {
+            System.out.println("Wear a swimming suit!");
+        } else if (season == "autumn") {
+            System.out.println("Wear a rain coat!");
+        } else {
+            System.out.println("Invalid season!");
+        }
+    }
+  }
