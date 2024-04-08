b77fa0cfada1989293f7e147c96b10b548463be3
efka87 <naanuri@hotmail.com>
2024-04-08 17:00:51 +0300

    Adding week5 code

---
+++ Projects/Week5LogicalOperators/Week5LogicalOperators/Program.cs
@@ -0,0 +1,15 @@
+﻿//AND - &&
+
+//true && true --> true (kõik tingimused peavad olema täidetud)
+//true && false --> false
+//false && true --> false
+//false && false --> false
+
+
+//OR - || (pipes)
+
+//true || true --> true
+//true || false --> true
+//false || true --> true
+//false || false --> false
+
